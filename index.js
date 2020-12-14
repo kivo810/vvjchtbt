652a15f1c860f829a2bba4f69a8dc6cbf15b2a05
kivo810 <kivo810@hotmail.com>
2020-12-14 01:32:18 +0100

    First part done

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const fetch = require('node-fetch')
 var port = process.env.PORT || config.get('PORT');
 
 const MOVIE_API = "http://www.omdbapi.com/?apikey=8df4f6a8"
+const WEATHER_API = "http://api.openweathermap.org/data/2.5/"
+const API_KEY = "&appid=8bebd0d8a0a3c015779632cb35e71641"
+
+const SEARCH_TEAM_API = "https://www.thesportsdb.com/api/v1/json/1/searchteams.php?"
+
+const UNITS = "&units=metric"
 
 const bot = new BootBot({
   accessToken: config.get('ACCESS_TOKEN'),
@@ -17,8 +23,58 @@ bot.on('message', (payload, chat) => {
 	console.log(`The user said: ${text}`);
 });
 
-bot.hear(['hello', 'hi'], (payload, chat) => {
-	chat.say('Hi! Movie?')
+bot.hear(['hi', 'hello'], (payload, chat) => {
+  chat.say('Hi! If you would like to know details about football team, type name of football team in style team *name*', {typing: true})
 });
+
+
+bot.hear(/team (.*)/i, (payload, chat, data) => {
+  chat.conversation((conversation) => {
+    const team = data.match[1];
+    console.log("Somebody asked about team " + team);
+    fetch(SEARCH_TEAM_API + 't=' + team)
+        .then(res => res.json())
+        .then(json => {
+          if (json.teams == null){
+            conversation.say("Could not find team with this name.", {typing:true});
+            conversation.end();
+          } else {
+            conversation.say('Found team ' + json.teams[0].strTeam, {typing:true});
+            setTimeout(() => {
+              conversation.say("Team was formed in "+json.teams[0].intFormedYear +" and its facebook site is " + json.teams[0].strFacebook, {typing: true});
+              handleMoreDetails(conversation, json);
+            }, 1000)
+          }
+        })
+  })
+})
+
+
+function handleMoreDetails(conversation, json) {
+  setTimeout(() => {
+    conversation.ask({
+      text: "Would you like to know what league and cups does this team play?",
+      quickReplies: ["Yes", "No"],
+      options: {typing: true}
+    }, (payload, conversation) => {
+      if (payload.message.text === "Yes") {
+        conversation.say(json.teams[0].strLeague, {typing:true});
+        if (json.teams[0].strLeague2 !== null){
+          conversation.say(json.teams[0].strLeague2, {typing:true});
+        }
+        handleStadiumDetails(conversation, json);
+        conversation.end();
+      } else {
+        conversation.say("Ok, ask me about a different team then.", {typing: true});
+        conversation.end();
+      }
+    });
+  }, 1000)
+}
+
+function handleStadiumDetails(conversation, json) {
+  console.log("im here")
+}
+
 
 bot.start(port);
