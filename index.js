15e561fc091b435b0a82fa3e498b55475f2bb3bf
kivo810 <kivo810@hotmail.com>
2020-12-14 11:09:08 +0100

    Second function added, some changes

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fetch = require('node-fetch')
 
 var port = process.env.PORT || config.get('PORT');
 
-const MOVIE_API = "http://www.omdbapi.com/?apikey=8df4f6a8"
-const WEATHER_API = "http://api.openweathermap.org/data/2.5/"
-const API_KEY = "&appid=8bebd0d8a0a3c015779632cb35e71641"
+// const MOVIE_API = "http://www.omdbapi.com/?apikey=8df4f6a8"
+// const WEATHER_API = "http://api.openweathermap.org/data/2.5/"
+// const API_KEY = "&appid=8bebd0d8a0a3c015779632cb35e71641"
 
 const SEARCH_TEAM_API = "https://www.thesportsdb.com/api/v1/json/1/searchteams.php?"
 
-const UNITS = "&units=metric"
+// const UNITS = "&units=metric"
 
 const bot = new BootBot({
   accessToken: config.get('ACCESS_TOKEN'),
@@ -63,17 +63,32 @@ function handleMoreDetails(conversation, json) {
           conversation.say(json.teams[0].strLeague2, {typing:true});
         }
         handleStadiumDetails(conversation, json);
-        conversation.end();
       } else {
         conversation.say("Ok, ask me about a different team then.", {typing: true});
         conversation.end();
       }
     });
-  }, 1000)
+  }, 2000)
 }
 
 function handleStadiumDetails(conversation, json) {
-  console.log("im here")
+  //console.log("im here")
+  setTimeout(() => {
+    conversation.ask({
+      text: "Would you like to know about stadium of this team??",
+      quickReplies: ["Yes", "No"],
+      options: {typing: true}
+    }, (payload, conversation) => {
+      if (payload.message.text === "Yes") {
+        conversation.say("Stadium is located in city " + json.teams[0].strStadiumLocation + " and have capacity of " + json.teams[0].intStadiumCapacity + " seats.", {typing:true});
+        conversation.say(json.teams[0].strStadiumDescription, {typing:true});
+        conversation.end();
+      } else {
+        conversation.say("Ok. Bye.", {typing: true});
+        conversation.end();
+      }
+    });
+  }, 2000)
 }
 
 
